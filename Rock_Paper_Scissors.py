68f1ae3abce74fdd833c65c9f669f54009f5e17b
Md Abdur Rabbi <164878932+abdurRabbi23@users.noreply.github.com>
2024-07-12 12:00:42 +0600

    Updated Rock_Paper_Scissors.py
    Just added some more functionalities to develop the game.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,6 +1,4 @@
 import random
-
-
 # we know the rule:-- rock > scissors;-- scissors > paper;-- paper > rock
 def condition_for_win(user, opponent):      # Checking the conditions if the player win or lose
     if ((user == 'r' and opponent == 's') or
@@ -8,6 +6,20 @@ def condition_for_win(user, opponent):      # Checking the conditions if the pla
             (user == 'p' and opponent == 'r')):
         return True
 
+def for_printing(user, computer):
+    if user == 'r':
+        print("Your choice is: 'Rock'")
+    elif user == 'p':
+        print("Your choice is: 'Paper'")
+    elif user == 's':
+        print("Your choice is: 'Scissors'")
+
+    if computer == 'r':
+        print("The opponent's choice is: 'Rock'")
+    elif computer == 'p':
+        print("The opponent's choice is: 'Paper'")
+    elif computer == 's':
+        print("The opponent's choice is: 'Scissors'")
 
 def play_Rock_Paper_Scissors():
     print()
@@ -20,65 +32,33 @@ def play_Rock_Paper_Scissors():
     # checking the conditions for user and opponent's choices.
     if user == computer:
         print()
-        if user == 'r':
-            print("Your choice is: 'Rock'")
-        elif user == 'p':
-            print("Your choice is: 'Paper'")
-        elif user == 's':
-            print("Your choice is: 'Scissors'")
-
-        if computer == 'r':
-            print("The opponent's choice is: 'Rock'")
-        elif computer == 'p':
-            print("The opponent's choice is: 'Paper'")
-        elif computer == 's':
-            print("The opponent's choice is: 'Scissors'")
-
+        for_printing(user, computer)
         print("It's a tie.. Please try again.")
 
     elif condition_for_win(user, computer):
         print()
-        if user == 'r':
-            print("Your choice is: 'Rock'")
-        elif user == 'p':
-            print("Your choice is: 'Paper'")
-        elif user == 's':
-            print("Your choice is: 'Scissors'")
-
-        if computer == 'r':
-            print("The opponent's choice is: 'Rock'")
-        elif computer == 'p':
-            print("The opponent's choice is: 'Paper'")
-        elif computer == 's':
-            print("The opponent's choice is: 'Scissors'")
-
+        for_printing(user, computer)
         print('Congratulations, You win!!...')
+
     else:
         print()
-        if user == 'r':
-            print("Your choice is: 'Rock'")
-        elif user == 'p':
-            print("Your choice is: 'Paper'")
-        elif user == 's':
-            print("Your choice is: 'Scissors'")
-
-        if computer == 'r':
-            print("The opponent's choice is: 'Rock'")
-        elif computer == 'p':
-            print("The opponent's choice is: 'Paper'")
-        elif computer == 's':
-            print("The opponent's choice is: 'Scissors'")
-
+        for_printing(user, computer)
         print('Bad luck.. You lose')
 
+    again_play()
+
+def again_play():
     print()
     print("Do you want to play again??\nIf yes then enter 'y' and if no then enter 'n'")
     user_again = input('Enter your option: ')
     if user_again == 'y':
         play_Rock_Paper_Scissors()
-    else:
+    elif user_again == 'n':
         print('Thanks for playing. Hope you enjoyed.')
         exit()
-
+    else:
+        print()
+        print('Invalid input, Please try with right one.')
+        again_play()
 
 play_Rock_Paper_Scissors()
